69649d90af44b40b0f972c62ca45fabe5adc1600
wekotsai <42733086+wekotsai@users.noreply.github.com>
2018-09-14 14:59:05 +0200

    changed icon and colour

---
+++ cityComponent.js
@@ -0,0 +1,30 @@
+Vue.component("city", {
+    created(){
+        fetch('https://api.myjson.com/bins/i8run')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(myJson => {
+            console.log(myJson);
+            this.cities = myJson.list;
+        });
+    },
+    template:`
+    <div class="gallery">
+        <div v-for="(city, index) in cities" class="city">       
+                <div>
+                   <p>{{ city.name }}</p>
+        
+                   <p>{{ city.weather[0].main }}</p>
+            
+                   <p>{{ city.main.temp }}</p>
+                </div>
+            
+        </div>
+    </div>`,
+    data(){
+        return{
+            cities: []
+        }
+    }
+})
