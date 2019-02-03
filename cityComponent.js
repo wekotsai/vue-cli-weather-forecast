b18d46930f4fb4da041b1e0e6ad6c2adaa52f925
Wéko <42733086+wekotsai@users.noreply.github.com>
2019-02-03 16:55:03 +0100

    removed comments

--- cityComponent.js
+++ cityComponent.js
@@ -5,13 +5,6 @@ Vue.component("city", {
             myImgUrl: ""
         }
     },
-    // beforeCreate() {
-    //     let imgArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg"];
-    //     let randomIndex = Math.floor( Math.random() * imgArray.length );
-    //     console.log(imgArray[ randomIndex ]);
-    //     this.myImgUrl = imgArray[ randomIndex ];
-    //     console.log(this.myImgUrl);
-    // },
     created(){
         fetch('https://api.myjson.com/bins/i8run')
         .then(function (response) {
@@ -27,16 +20,4 @@ Vue.component("city", {
             <oneCity :city="city" :key="index"></oneCity>
         </div>
     </div>`,
-    // methods: {
-    //     getImgUrl(){
-    //         let imgArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg"];
-    //         let randomIndex = Math.floor( Math.random() * imgArray.length );
-    //         console.log(imgArray[ randomIndex ]);
-    //         this.myImgUrl = imgArray[ randomIndex ];
-    //     }
-    
 })
-
-//
-
-//background-image: url("img1.jpg")
